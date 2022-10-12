7d2c70ece9c39ecfb27471b9e2b04dc9a11ba94e
Linnea Ehlin <linnea.ehlin@awacademy.training>
2022-10-12 12:21:32 +0200

    ändrat text och färg på frontpage.html och product.html

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO PRODUCT (NAME, PRICE, CATEGORY, IMAGE) VALUES
 ('Tale of Sushi',349,'Fantasy', '/images/sushibook.png'),
 ('Pippi Långstrump',150,'Fantasy', '/images/pippi.jpg'),
-('the Hitchhikers Guide to the Galaxy',249,'Fantasy', '/images/thehitchhikers.jpg'),
-('the Iliad',34999,'Fantasy', '/images/theiliad.jpg');
+('The Hitchhikers Guide to the Galaxy',249,'Fantasy', '/images/thehitchhikers.jpg'),
+('The Iliad',34999,'Fantasy', '/images/theiliad.jpg');
 
 
 INSERT INTO SITEUSER (USERNAME, PASSWORD) VALUES
