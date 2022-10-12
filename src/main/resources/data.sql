87d7e908d397c45d0d0bbd4d75a33d2ba4bee597
Linnea Ehlin <linnea.ehlin@awacademy.training>
2022-10-12 11:52:00 +0200

    added sushibook-pic

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO PRODUCT (NAME, PRICE, CATEGORY, IMAGE) VALUES
-('Tale of Sushi',349,'Fantasy', '/images/sushi.png'),
+('Tale of Sushi',349,'Fantasy', '/images/sushibook.png'),
 ('Pippi Långstrump',150,'Fantasy', '/images/pippi.jpg'),
 ('the Hitchhikers Guide to the Galaxy',249,'Fantasy', '/images/thehitchhikers.jpg'),
 ('the Iliad',34999,'Fantasy', '/images/theiliad.jpg');
