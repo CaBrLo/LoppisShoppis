d3eaa445335c7e9c447ae2b426a9e7e99249484e
CarolineBranderLöf <caroline.branderlof@awacademy.training>
2022-10-16 20:57:15 +0200

    Products added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,10 @@ INSERT INTO PRODUCT (NAME, PRICE, DESCRIPTION, CATEGORY, IMAGE) VALUES
 ('The Iliad',34999, 'Författare: Homeros. Utgiven: 1715. Översatt av Alexander Pope. Läderpärm. Skick: Välanvänd.','History', '/images/theiliad.jpg'),
 ('Häxa - dekoration', 99, 'Dekorationshäxa.', 'Halloween', '/images/halloweenhaxa.png'),
 ('Pumpa - värmeljuslykta', 149, 'Värmeljuslykta i form av en Halloween-pumpa.', 'Halloween', '/images/pumpalykta.png'),
-('Skelett - dekoration', 299, 'Dekorativt och verklighetstroget skelett vid namn Benjamin, ca 160 cm.', 'Halloween', '/images/skelett.png');
+('Skelett - dekoration', 299, 'Dekorativt och verklighetstroget skelett vid namn Benjamin, ca 160 cm.', 'Halloween', '/images/skelett.png'),
+('Demospöke - dekoration och husspöke', 9999, 'Sällskapssjukt husspöke som ogillar teknik. Tenderar att dyka upp under demos för att sabotera. (Svår att fota, bild från tidigare ägare).', 'Halloween', '/images/spoke.jpg'),
+('Tant Grön, Tant Brun och Tant Gredelin',199,'Författare: Elsa Beskow. Utgiven: 1920. Skick: Gott.', 'Barnbok', '/images/tantgron.jpg'),
+('1984',99,'Författare: George Orwell. Utgiven: 1980. Skick: Ny.', 'Sci-Fi', '/images/1984.jpg');
 
 
 INSERT INTO SITEUSER (USERNAME, PASSWORD) VALUES
